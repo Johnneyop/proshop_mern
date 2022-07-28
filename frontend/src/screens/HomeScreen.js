83af0d2889542defaa355db1d4087d8b359dc1b7
johnneyop <johnneyop@gmail.com>
2022-07-28 15:55:47 -0500

    Configure HomeScreen
    1. Change how HomeScreen pull product from frontend directory from backend.
    2. Add useState & useEffect to pull products from backend
    3. import axios library to fetch json data from products.js backend
    4. Configured frontend Json package to use proxy "http://127.0.0.1:5000" to start backend and frontend together

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
-import products from "../products";
-import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products'); //destructure from res.data
+
+      setProducts(data); // changes useState([])
+    };
+
+    fetchProducts();
+  }, []); // [] dependencies that will fire when useEffect is called
+
   return (
     <>
       <h1>Latest Products</h1>
